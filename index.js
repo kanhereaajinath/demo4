305327799191255b016250363ec686e6fceb28bc
kanhereaajinath <aajinathkanhere@gmail.com>
2023-09-27 17:22:42 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express =require('express')
 const app=express()
 app.get('/',(req,res)=>{
-    res.send("hello world")
+    res.send("hellohh world")
+
 
 })
 const PORT=process.env.PORT||4000;
